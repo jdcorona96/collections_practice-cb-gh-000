4916a18568760d74a7bae6e4e7ac514836d4d7c8
Joseph Corona <josed.hc96@gmail.com>
2018-11-15 00:48:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,4 +20,28 @@ def swap(arr, ind1, ind2)
   arr[ind1], arr[ind2] = arr[ind2], arr[ind1]
   arr
 end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(words)
+  words.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+  
+  
   
