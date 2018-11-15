ffc6989164b7a1499198101765bf40041a5520cd
Joseph Corona <josed.hc96@gmail.com>
2018-11-15 00:30:50 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  swap(array,1,2)
+end
+
+def swap(arr, ind1, ind2)
+  arr[ind1], arr[ind2] = arr[ind2], arr[ind1]
+  arr
+end
+  
